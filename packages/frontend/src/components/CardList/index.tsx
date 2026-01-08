48f29f0e4d7c6242f61a72654d82ed5140ac605a
Cody Dwyer <codydwyer@gmail.com>
2026-01-08 12:16:26 -0800

    adding cors for gql and fetching all cards

--- packages/frontend/src/components/CardList/index.tsx
+++ packages/frontend/src/components/CardList/index.tsx
@@ -1,5 +1,31 @@
-import type { ReactElement } from 'react';
+import { type ReactElement } from 'react';
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client/react';
 
-export default ():ReactElement => <>
-  <h2>Card List</h2>      
-</>
+const GET_CARDS = gql`
+  query GetCards {
+    cards {
+      id
+      arcana
+      image
+      name
+      number
+      suit
+    }
+  }
+`;
+
+export default ():ReactElement => {
+  const { loading, error, data } = useQuery(GET_CARDS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p style={{ color: "red" }}>Error: {error.message}</p>;
+
+  return <>
+    <h2>Card List</h2>   
+    {data && data.cards.map(({ id, name }: { id: string; name: string }) => (
+      <div key={id}>
+        <p>{name}</p>
+      </div>
+    ))}   
+  </>
+}
--- packages/graphql/index.js
+++ packages/graphql/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import { createHandler } from 'graphql-http/lib/use/express';
 import { ruruHTML } from 'ruru/server';
+import cors from 'cors';
 
 import schema from './schema/index.js';
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
+
+app.use(cors(corsOptions));
 
 app.all('/graphql', createHandler({ schema}));
 
