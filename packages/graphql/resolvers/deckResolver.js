180159aeffdbeb4b3c2ebdba236680159e0f2814
Cody Dwyer <codydwyer@gmail.com>
2026-01-20 14:57:13 -0800

    adding brute force joining of decks_cards table on cards field

--- packages/graphql/resolvers/deckResolver.js
+++ packages/graphql/resolvers/deckResolver.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 import { dbUrl } from '../conf.js';
+import cardResolver from './cardResolver.js';
 
-const deckResolver = (parent, args) => {
-  const url = `${dbUrl}/decks/${args.id}`; 
-  console.log('Fetching deck from URL:', url);
-  return axios.get(url).then(response => response.data);
+const deckResolver = async (parent, args) => {
+  const deckUrl = `${dbUrl}/decks/${args.id}`; 
+  const deckCardsUrl = `${dbUrl}/decks_cards?deckId=${args.id}`; 
+
+  console.log('Fetching deck from URL:', deckUrl);
+  console.log('Fetching deck cards from URL:', deckCardsUrl);
+
+  const deck = await axios.get(deckUrl).then(response => response.data);
+  const deckCards = await axios.get(deckCardsUrl).then(response => response.data);
+  const deck_cards = deckCards.map(dc => dc.cardId);
+  deck.cards = await Promise.all(deck_cards.map(cardId => cardResolver(null, { id: cardId })));
+  
+  return deck;
 }
 
 export default deckResolver;
--- packages/graphql/types/deck.js
+++ packages/graphql/types/deck.js
@@ -1,11 +1,13 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLObjectType, GraphQLString, GraphQLList } from 'graphql';
+import CardType from './card.js';
 
 const DeckType = new GraphQLObjectType({
   name: 'Deck',
   fields: {
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
+    description: { type: GraphQLString },
+    cards: { type: new GraphQLList(CardType) } // This can be modified to a list type if needed
   }
 });
 
