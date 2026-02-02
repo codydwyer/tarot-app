4249c786096bfd03e69b0c4566b6cc40626c2413
Cody Dwyer <codydwyer@gmail.com>
2026-02-02 11:07:25 -0800

    updating code coverage

--- packages/frontend/jest.config.ts
+++ packages/frontend/jest.config.ts
@@ -1,7 +1,20 @@
-export default {
+import type {Config} from 'jest';
+
+const config: Config = {
+  
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(ts|tsx)?$': ['ts-jest', { diagnostics: { ignoreCodes: ['TS151001'] } }],
-  }
-}
+  },
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: 50,
+    },
+  },
+};
+
+export default config;
